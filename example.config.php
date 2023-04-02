4ea93bd4ef09bbe2db92e47915722eec668c2e0f
redboo <garda1@mail.ru>
2023-04-03 04:31:27 +0500

    add config and PHPMailer

---
+++ example.config.php
@@ -0,0 +1,6 @@
+<?php
+
+define('MAIL_FROM', 'your@mail.com');
+// Получение пароля https://support.google.com/accounts/answer/185833?hl=ru
+define('MAIL_FROM_PASSWORD', 'password');
+define('MAIL_TO', 'your@mail.com');
--- send.php
+++ send.php
@@ -1,57 +1,68 @@
 <?php
-
 use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
 
-require './PHPMailer/src/Exception.php';
-require './PHPMailer/src/PHPMailer.php';
-require './PHPMailer/src/SMTP.php';
+if (isset($_POST) && !empty($_POST)) {
 
-// Собираем текст сообщения
-$message = 'Пользователь ' . $_POST['phone'] . " заказал расчет услуг.\n\n";
-$type = [];
-if (isset($_POST['cms'])) $type[] = 'Система управления сайтом';
-if (isset($_POST['logo'])) $type[] = 'Разработка логотипа';
-if (isset($_POST['catalog'])) $type[] = 'Каталог товаров';
-if (isset($_POST['support'])) $type[] = 'Поддержка 1 год';
-if (isset($_POST['landing'])) $type[] = 'Многостраничный лендинг';
-if (isset($_POST['payment'])) $type[] = 'Оплата на сайте';
-if (isset($_POST['analysis'])) $type[] = 'Конкурентный анализ';
-if (isset($_POST['longread'])) $type[] = 'Лонгрид';
-if (isset($_POST['writing'])) $type[] = 'Написение текстов';
-if (isset($_POST['specification'])) $type[] = 'Помощь с техническим заданием';
+	require('config.php');
+	require './PHPMailer/src/Exception.php';
+	require './PHPMailer/src/PHPMailer.php';
+	require './PHPMailer/src/SMTP.php';
 
-if (!empty($type)) {
-	$message .= "Выбранные услуги:\n<ul>";
-	foreach ($type as $item) {
-		$message .= '<li>' . $item . '</li>';
+	// Собираем текст сообщения
+	$message = 'Пользователь ' . $_POST['phone'] . " заказал расчет услуг.\n\n";
+	$type = [];
+	if (isset($_POST['cms']))
+		$type[] = 'Система управления сайтом';
+	if (isset($_POST['logo']))
+		$type[] = 'Разработка логотипа';
+	if (isset($_POST['catalog']))
+		$type[] = 'Каталог товаров';
+	if (isset($_POST['support']))
+		$type[] = 'Поддержка 1 год';
+	if (isset($_POST['landing']))
+		$type[] = 'Многостраничный лендинг';
+	if (isset($_POST['payment']))
+		$type[] = 'Оплата на сайте';
+	if (isset($_POST['analysis']))
+		$type[] = 'Конкурентный анализ';
+	if (isset($_POST['longread']))
+		$type[] = 'Лонгрид';
+	if (isset($_POST['writing']))
+		$type[] = 'Написение текстов';
+	if (isset($_POST['specification']))
+		$type[] = 'Помощь с техническим заданием';
+
+	if (!empty($type)) {
+		$message .= "Выбранные услуги:\n<ul>";
+		foreach ($type as $item) {
+			$message .= '<li>' . $item . '</li>';
+		}
+		$message .= "</ul>";
 	}
-	$message .= "</ul>";
-}
 
-$mail = new PHPMailer;
+	$mail = new PHPMailer;
 
-// Настраиваем SMTP соединение
-$mail->isSMTP();
-$mail->Host = 'smtp.gmail.com';
-$mail->SMTPAuth = true;
-$mail->Username = 'YourEmail@gmail.com';
-// https://support.google.com/accounts/answer/185833?hl=ru получение пароля
-$mail->Password = 'asdfasdfasdf';
-$mail->SMTPSecure = 'ssl';
-$mail->Port = 465;
+	// Настраиваем SMTP соединение
+	$mail->isSMTP();
+	$mail->Host = 'smtp.gmail.com';
+	$mail->SMTPAuth = true;
+	$mail->Username = MAIL_FROM;
+	$mail->Password = MAIL_FROM_PASSWORD;
+	$mail->SMTPSecure = 'ssl';
+	$mail->Port = 465;
 
-// Устанавливаем параметры письма
-$mail->CharSet = "utf-8";
-$mail->setFrom('YourEmail@gmail.com', 'Самурай');
-$mail->addAddress('to@mail.ru');
-$mail->Subject = 'Заказ услуг';
-$mail->Body = nl2br($message); // Заменяем переносы строк на <br> для корректного отображения в html-письме
-$mail->isHTML(true); // Устанавливаем формат письма - html
+	// Устанавливаем параметры письма
+	$mail->CharSet = "utf-8";
+	$mail->setFrom(MAIL_FROM, 'Самурай');
+	$mail->addAddress(MAIL_TO);
+	$mail->Subject = 'Заказ услуг';
+	$mail->Body = nl2br($message);
+	$mail->isHTML(true);
 
-if (!$mail->send()) {
-	echo 'Message could not be sent.';
-	echo 'Mailer Error: ' . $mail->ErrorInfo;
-} else {
-	echo 'Спасибо, что воспользовались нашими услугами. Скоро с Вами свяжутся наши специалисты.';
+	if (!$mail->send()) {
+		echo 'Message could not be sent.';
+		echo 'Mailer Error: ' . $mail->ErrorInfo;
+	} else {
+		echo 'Спасибо, что воспользовались нашими услугами. Скоро с Вами свяжутся наши специалисты.';
+	}
 }
